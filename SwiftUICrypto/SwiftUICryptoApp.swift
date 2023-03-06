bb1d11b33bf11b60c1725b74b99a36123e20de6b
Mohammed Osman <mohammed.osman@vianeos.com>
2023-03-06 15:03:02 +0400

    Initial Commit

---
+++ SwiftUICrypto/SwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoApp.swift
+//  SwiftUICrypto
+//
+//  Created by Mohammed Osman on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
